655d03c1a8e265b297b27bff27e02ee4127053d3
23solo <yagami.naruto23@gmail.com>
2023-06-22 12:39:48 +0530

    [REFACTOR] added statement for problems

--- LinkedList/middleNode.cpp
+++ LinkedList/middleNode.cpp
@@ -1,44 +1,52 @@
+/*
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
 typedef long long ll;
 typedef vector<int> vi;
 typedef vector<long long> vll;
-typedef pair<int,int> pi;
+typedef pair<int, int> pi;
 typedef unordered_map<int, int> um;
-typedef vector<vector<int> > vvi;
+typedef vector<vector<int>> vvi;
 
 #define F first
 #define S second
 #define PB push_back
 #define MP make_pair
-#define FOR(i, N) for(int i =0; i<N; i++)
+#define FOR(i, N) for (int i = 0; i < N; i++)
 // Creating a ListNode
-class ListNode {
-  public:
+class ListNode
+{
+public:
   int value;
-  ListNode* next;
+  ListNode *next;
 };
 
-ListNode* middleNode(ListNode* fast) {
-        if(fast == NULL || fast -> next == NULL){
-            return fast;
-        }
-        ListNode* slow = fast;
-        while(fast != NULL && fast -> next != NULL){
-            fast = fast -> next -> next;
-            slow = slow -> next;
-        }
-        return slow;
-    }
+ListNode *middleNode(ListNode *fast)
+{
+  if (fast == NULL || fast->next == NULL)
+  {
+    return fast;
+  }
+  ListNode *slow = fast;
+  while (fast != NULL && fast->next != NULL)
+  {
+    fast = fast->next->next;
+    slow = slow->next;
+  }
+  return slow;
+}
 
 void solve()
 {
-  ListNode* head;
-  ListNode* one = NULL;
-  ListNode* two = NULL;
-  ListNode* three = NULL;
-
+  ListNode *head;
+  ListNode *one = NULL;
+  ListNode *two = NULL;
+  ListNode *three = NULL;
 
   // allocate 3 ListNodes in the heap
   one = new ListNode();
@@ -57,20 +65,21 @@ void solve()
 
   // print the linked list value
 
-  ListNode* middle = middleNode(one);
-  cout<<middle -> value;
+  ListNode *middle = middleNode(one);
+  cout << middle->value;
 }
 
-int main() {
-	ios::sync_with_stdio(0);
-    cin.tie(0);
-	int t = 1;
-	// cin >> t;
-	while(t--) {
-		solve();
-		cout<<"\n";
-	}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t = 1;
+  // cin >> t;
+  while (t--)
+  {
+    solve();
+    cout << "\n";
+  }
 
-	return 0;
+  return 0;
 }
-
--- LinkedList/reverseLinkedList.cpp
+++ LinkedList/reverseLinkedList.cpp
@@ -1,51 +1,55 @@
+/*
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
 typedef long long ll;
 typedef vector<int> vi;
 typedef vector<long long> vll;
-typedef pair<int,int> pi;
+typedef pair<int, int> pi;
 typedef unordered_map<int, int> um;
-typedef vector<vector<int> > vvi;
+typedef vector<vector<int>> vvi;
 
 #define F first
 #define S second
 #define PB push_back
 #define MP make_pair
-#define FOR(i, N) for(int i =0; i<N; i++)
+#define FOR(i, N) for (int i = 0; i < N; i++)
 // Creating a ListNode
-class ListNode {
-  public:
+class ListNode
+{
+public:
   int value;
-  ListNode* next;
+  ListNode *next;
 };
 
-ListNode* reverseLinkedList(ListNode *head){
-  if (head -> next == NULL)
+ListNode *reverseLinkedList(ListNode *head)
+{
+  if (head->next == NULL)
   {
     return head;
   }
-  ListNode* prev = head;
-  head = head -> next;
-  prev -> next = NULL;
-  while (head != NULL){
-    ListNode* temp;
-    temp = head -> next;
-    head -> next = prev;
+  ListNode *prev = head;
+  head = head->next;
+  prev->next = NULL;
+  while (head != NULL)
+  {
+    ListNode *temp;
+    temp = head->next;
+    head->next = prev;
     prev = head;
     head = temp;
   }
   return prev;
-  
 }
 
 void solve()
 {
-  ListNode* head;
-  ListNode* one = NULL;
-  ListNode* two = NULL;
-  ListNode* three = NULL;
-
+  ListNode *head;
+  ListNode *one = NULL;
+  ListNode *two = NULL;
+  ListNode *three = NULL;
 
   // allocate 3 ListNodes in the heap
   one = new ListNode();
@@ -65,22 +69,24 @@ void solve()
   // print the linked list value
 
   head = reverseLinkedList(one);
-  while (head != NULL) {
+  while (head != NULL)
+  {
     cout << head->value;
     head = head->next;
   }
 }
 
-int main() {
-	ios::sync_with_stdio(0);
-    cin.tie(0);
-	int t = 1;
-	// cin >> t;
-	while(t--) {
-		solve();
-		cout<<"\n";
-	}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t = 1;
+  // cin >> t;
+  while (t--)
+  {
+    solve();
+    cout << "\n";
+  }
 
-	return 0;
+  return 0;
 }
-
