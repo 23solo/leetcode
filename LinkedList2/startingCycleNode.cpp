bd3d5f332b43d2e60a5c74ee956a4998e043af9f
23solo <yagami.naruto23@gmail.com>
2023-06-27 00:18:00 +0530

    [FEAT] solution for startingNodeCycle

---
+++ LinkedList2/startingCycleNode.cpp
@@ -0,0 +1,98 @@
+/*
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef pair<int, int> pi;
+typedef unordered_map<int, int> um;
+typedef vector<vector<int>> vvi;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define FOR(i, N) for (int i = 0; i < N; i++)
+// Creating a ListNode
+class ListNode
+{
+public:
+  int val;
+  ListNode *next;
+};
+
+ListNode *addNode()
+{
+  ListNode *vNodes;
+  int m;
+  cout << "Enter size of linked list ";
+  cin >> m;
+
+  ListNode *list1, *temp;
+  FOR(i, m)
+  {
+    int val;
+    cin >> val;
+    ListNode *newNode = new ListNode();
+    newNode->val = val;
+    if (i == 0)
+    {
+      list1 = newNode;
+      temp = list1;
+      continue;
+    }
+    temp->next = newNode;
+    temp = temp->next;
+  }
+  temp->next = list1->next;
+  return list1;
+}
+
+ListNode *detectCycle(ListNode *slow)
+{
+  ListNode *fast = slow, *base = slow;
+  while (fast != NULL && fast->next != NULL)
+  {
+    fast = fast->next->next;
+    slow = slow->next;
+    if (slow == fast)
+    {
+      while (slow != base)
+      {
+        slow = slow->next;
+        base = base->next;
+      }
+      return slow;
+    }
+  }
+  return NULL;
+}
+
+void solve()
+{
+  ListNode *list = addNode();
+
+  cout << detectCycle(list)->data;
+}
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t = 1;
+  // cin >> t;
+  while (t--)
+  {
+    solve();
+    cout << "\n";
+  }
+
+  return 0;
+}
